133a47c2f5001dd737ecc6d24ab0dd99ddf81707
sanketshirolkar <snktshirolkar@gmail.com>
2020-08-05 11:47:25 +0000

    Added code perform addition

--- SampleCode/Src/sample.c
+++ SampleCode/Src/sample.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 int main()
 {
+  int n1 = 0;
+  int n2 = 0;
+  int sum = 0;
+
+  printf("Enter n1: ");
+  scanf("%d", &n1);
+
+  printf("Enter n2: ");
+  scanf("%d", &n2);
+
+  sum = n1 + n2;
+
+  printf("Sum: %d\n", sum);
   return 0;
 }
