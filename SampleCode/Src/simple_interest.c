4cfa628a67e677db1fb3e3fa991917b376eaa5fb
sanketshirolkar <snktshirolkar@gmail.com>
2020-08-29 09:14:20 +0000

    Program to calculate simple interest is re written so that we can get inputs through keyboard.

--- SampleCode/Src/simple_interest.c
+++ SampleCode/Src/simple_interest.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
-
 int main ()
 {
 int p, n;
 float r, si;
-
-p = 135; 
-n = 1;
-r = 1;
-
+printf ("Enter the values of p, n, r");
+scanf ("%d%d%f", &p, &n, &r);
 si = p*n*r/100;
 printf ("%f", si);
-return 0;
-
-
-  
+return 0; 
 }
